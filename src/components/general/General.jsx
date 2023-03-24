202a44c33e3058eb84e57218f7eba593222cc5c2
shivam6862 <shivam6862mau@gmail.com>
2023-03-24 11:49:04 +0530

    backdrop

--- src/components/general/General.jsx
+++ src/components/general/General.jsx
@@ -11,61 +11,60 @@ const General = ({ isLoading, datas, titlePage }) => {
   return (
     <div className={classes.col}>
       <div className={classes.row}>
-        {eachNews ? (
+        {eachNews && (
           <DetailNews
             link={linkActive}
             setEachNews={setEachNews}
             newsActive={newsActive}
           />
-        ) : (
-          <div className={classes.working}>
-            <div className={classes.heading}>{titlePage}</div>
-            <div className={classes.works}>
-              {isLoading || datas.status == "error" ? (
-                <LoadingSpinner />
-              ) : (
-                <>
-                  {datas.items.map((data, index) => (
-                    <div key={index} className={classes.work}>
-                      <div className={classes.book_data}>
+        )}
+        <div className={classes.working}>
+          <div className={classes.heading}>{titlePage}</div>
+          <div className={classes.works}>
+            {isLoading || datas.status == "error" ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                {datas.items.map((data, index) => (
+                  <div key={index} className={classes.work}>
+                    <div className={classes.book_data}>
+                      <div className={classes.work_info}>
+                        {data.pubDate.slice(0, 10)}
+                      </div>
+                      <div className={classes.about}>{data.author}</div>
+                      <div>
+                        <div className={classes.work_body}>
+                          <img
+                            src={data.enclosure.link}
+                            className={classes.img_fluid}
+                          />
+                        </div>
                         <div className={classes.work_info}>
-                          {data.pubDate.slice(0, 10)}
+                          {data.title.slice(0, 50)}...
                         </div>
-                        <div className={classes.about}>{data.author}</div>
-                        <div>
-                          <div className={classes.work_body}>
-                            <img
-                              src={data.enclosure.link}
-                              className={classes.img_fluid}
-                            />
-                          </div>
-                          <div className={classes.work_info}>
-                            {data.title.slice(0, 50)}...
-                          </div>
-                        </div>
-                        <div className={classes.overview}>
-                          <div className={classes.back_content}>
-                            <div className={classes.button}>
-                              <Button
-                                onClick={() => {
-                                  setLinkAcctive(data.link);
-                                  setEachNews(true);
-                                  setNewsActive(data);
-                                }}
-                              >
-                                Read more
-                              </Button>
-                            </div>
+                      </div>
+                      <div className={classes.overview}>
+                        <div className={classes.back_content}>
+                          <div className={classes.button}>
+                            <Button
+                              onClick={() => {
+                                setLinkAcctive(data.link);
+                                setEachNews(true);
+                                setNewsActive(data);
+                              }}
+                            >
+                              Read more
+                            </Button>
                           </div>
                         </div>
                       </div>
                     </div>
-                  ))}
-                </>
-              )}
-            </div>
+                  </div>
+                ))}
+              </>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
